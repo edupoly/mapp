7e441753d0eb73bd6ee01862e71bf61baf8a08b4
edupoly <info.edupoly@gmail.com>
2023-01-23 20:58:42 +0530

    Counter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Counter from './Counter';
 
 function App() {
   return (
     <div className='border border-2 p-2 border-danger'>
-      
+      <Counter initialCount={100}></Counter>
+      <Counter initialCount={200}></Counter>
+      <h1 style={{color:'red'}}>Edupoly</h1>
+      <h1 style={{color:'green'}}>Praveen</h1>
     </div>
   );
 }
---
+++ src/Counter.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Counter(props) {
+  console.log(props)
+  var [count,setCount] = React.useState(100)
+  function inc(){
+    setCount(count+1)
+  }
+  function dec(){
+    setCount(count-1)
+  }
+  return (
+    <div className='border border-3 p-2 border-success'>
+      <h1>Counter</h1>
+      <h2>Count:{count}</h2>
+      <button onClick={inc}>Increment</button>
+      <button onClick={dec}>Decrement</button>
+    </div>
+  )
+}
+
+export default Counter
