7994becfbb1c4d9d212592128d913f764d339af1
edupoly <info.edupoly@gmail.com>
2023-01-30 19:43:47 +0530

    usecallbackhook

--- src/Todolist.jsx
+++ src/Todolist.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import Todo from './Todo';
 
 function Todolist() {
+  console.log("Todolist rendered")
   var [todos,setTodos] = React.useState([
     {
       title:'clear bills',
@@ -23,9 +25,25 @@ function Todolist() {
       status:true
     },
   ])
+  function addTask(){
+    var newtask = document.getElementById('task').value;
+    setTodos([...todos,{title:newtask,status:false}])
+  }
+  var done=React.useCallback(function(i){
+    var temp = [...todos];
+    temp[i].status=!temp[i].status;
+    setTodos([...temp])
+  },[])
   return (
     <div className='border border-2 p-2 m-2'>
       <h1>Todolist</h1>
+      <input type="text" id='task' />
+      <button onClick={addTask}>Add Task</button>
+      {
+        todos.map((todo,i)=>{
+          return <Todo title={todo.title} status={todo.status} done={done} i={i}></Todo>
+        })
+      }
     </div>
   )
 }
