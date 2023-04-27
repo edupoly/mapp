12793aff1e144370972a229a051d0872008813c8
edupoly <info.edupoly@gmail.com>
2023-04-27 12:58:52 +0530

    asd

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
-import Counter from './Counter';
 import store from './store/store';
 import { Provider } from 'react-redux';
-import Todolist from './Todolist';
 import Quiz from './Quiz';
 function App() {
   return (
     <Provider store={store}>
       <div className='border border-2 p-2 border-success'>
         <Quiz></Quiz>
-        <Counter></Counter>
-        <Todolist></Todolist>
       </div>
     </Provider>
   );
